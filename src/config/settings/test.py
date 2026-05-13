0e06b7ccfdc3fe7ea9b809d899f035a482c43a10
Moustapha KINTY <kintymoustapha@gmail.com>
2026-05-13 14:22:10 +0200

    GitHub Actons: test & quality

---
+++ src/config/settings/test.py
@@ -0,0 +1,22 @@
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = "test-secret"
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+CELERY_TASK_ALWAYS_EAGER = True
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
