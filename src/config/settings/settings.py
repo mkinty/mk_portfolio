698b78e834cfd673f9a0d75f662e13f5d10d794d
Moustapha KINTY <kintymoustapha@gmail.com>
2026-05-15 21:57:36 +0200

    deploy to staging

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent  # src/
+
 PROJECT_DIR = BASE_DIR.parent  # portfolio/
 
 # Charge le fichier .env
